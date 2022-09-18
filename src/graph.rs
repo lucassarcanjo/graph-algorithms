f4e17b7290b1d87578c51c4cad18b27e96fa6d0e
Lucas Arcanjo <lucassarcanjo@gmail.com>
2022-09-18 20:11:37 -0300

    feat: add depth first search

--- src/graph.rs
+++ src/graph.rs
@@ -1,11 +1,20 @@
 use std::{collections::{LinkedList, VecDeque}, borrow::Borrow};
 
 #[derive(Clone)]
-pub struct Node {
+pub struct NodeBfs {
   name: &'static str,
   // parent_name: &'static str,
   // level: i32,
   index: i32,
+  visited: bool,
+}
+
+#[derive(Clone)]
+pub struct NodeDfs {
+  name: &'static str,
+  discovery_time: i32,
+  // finish_time: i32,
+  // parent_name: &'static str,
   visited: bool,
 }
 
@@ -83,25 +92,17 @@ impl Graph {
   }
 
   pub fn bfs(&mut self, start_node_name: &str) {
-    println!("Starting from vertex {:?}\n", start_node_name);
-
-    // check if the start node is valid
-    let start_node_result = self.get_node_position_by_name(start_node_name);
-
-    if start_node_result == None {
-      println!("Invalid node, you must to add an edge with name: {} or add a node manually", start_node_name);
-      return;
-    }
+    self.check_is_valid_node(start_node_name);
 
     // initializations
     let mut counter = 0;
-    let mut queue: VecDeque<Node> = VecDeque::new();
-    let mut node_list: Vec<Node> = Vec::with_capacity(self.adjacency_list.len());
+    let mut queue: VecDeque<NodeBfs> = VecDeque::new();
+    let mut node_list: Vec<NodeBfs> = Vec::with_capacity(self.adjacency_list.len());
 
     // node initialization
     for list_item in &self.adjacency_list {
       node_list.push(
-        Node { 
+        NodeBfs { 
           index: 0,
           // level: 0,
           // parent_name: "",
@@ -110,7 +111,7 @@ impl Graph {
         })
     };
 
-    fn get_remaining_items(node_list: &mut Vec<Node>, counter: i32, start_node_name: String) -> Option<&mut Node>{
+    fn get_remaining_items(node_list: &mut Vec<NodeBfs>, counter: i32, start_node_name: String) -> Option<&mut NodeBfs>{
       if counter == 0 {
         return node_list.iter_mut().find(|x| x.name == start_node_name);
       }
@@ -128,7 +129,7 @@ impl Graph {
       while queue.len() > 0 {
         counter += 1;
         let current_node = queue.pop_front().unwrap();
-        let current_node_neighbors = self.adjacency_list.iter().find(|&x| x.node == current_node.name).unwrap().list.borrow();
+        let current_node_neighbors = self.get_node_neighbors(current_node.name);
 
         print!("{} ", current_node.name);
 
@@ -146,6 +147,84 @@ impl Graph {
       }
     }
 
+  }
+
+  pub fn dfs(&mut self, start_node_name: &str) {
+    self.check_is_valid_node(start_node_name);
+
+    let mut counter = 0;
+    let mut node_list: Vec<NodeDfs> = Vec::with_capacity(self.adjacency_list.len());
+    let mut stack: Vec<&str> = Vec::new(); 
+
+    // node initialization
+    for list_item in &self.adjacency_list {
+      node_list.push(
+        NodeDfs {
+          name: list_item.node,
+          discovery_time: 0,
+          // finish_time: 0,
+          // parent_name: "",
+          visited: false,
+        })
+    };
+
+    while let Some(root_node) = get_remaining_items(&mut node_list, counter, start_node_name.to_owned()) {
+      // add node to the stack
+      stack.push(root_node.name);
+  
+      while !stack.is_empty() {
+        counter += 1;
+
+        let current_node_name = stack.pop().unwrap();
+        let current_node = node_list.iter_mut().find(|x| x.name == current_node_name).unwrap();
+  
+        // check if the stack contain a node already visited
+        if !current_node.visited {
+          print!("{} ", current_node.name);
+          current_node.visited = true;
+          current_node.discovery_time = counter;
+        }
+  
+        // get all adjacent nodes
+        let neighbors = self.get_node_neighbors(current_node.name);
+  
+        for &neighbor_name in neighbors {
+          let neighbor_node = node_list.iter_mut().find(|x| x.name == neighbor_name).unwrap();
+  
+          // push not visited adjacents to the stack
+          if !neighbor_node.visited {
+            stack.push(neighbor_node.name);
+          }
+        }
+      }
+
+    }
+
+    fn get_remaining_items (node_list: &mut Vec<NodeDfs>, counter: i32, start_node_name: String) -> Option<&mut NodeDfs> {
+      if counter == 0 {
+        return node_list.iter_mut().find(|x| x.name == start_node_name);
+      }
+
+      return node_list.iter_mut().filter(|x| x.discovery_time == 0).next();
+    }
+
+  }
+
+  fn get_node_neighbors(&mut self, node_name: &str) -> &LinkedList<&str> {
+    return self.adjacency_list.iter().find(|&x| x.node == node_name).unwrap().list.borrow();
+  }
+
+  fn check_is_valid_node(&mut self, name: &str) {
+    println!("Starting from vertex {:?}\n", name);
+
+    // check if the start node is valid
+    let start_node_result = self.get_node_position_by_name(name);
+
+    if start_node_result == None {
+      println!("Invalid node, you must to add an edge that connects to '{}' or add that node manually", name);
+
+      std::process::exit(1);
+    }
   }
 
   fn get_node_position_by_name(&mut self, name: &str) -> Option<usize> {
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ mod graph;
 use graph::Graph;
 
 fn main() {
-    println!("Hi, let's go to run BFS on a graph 🔥");
+    println!("Hi, let's go to run BFS and DFS on a graph 🔥");
 
     let mut d = Graph::new(false);
 
-    d.add_edge("A", "B");
-    d.add_edge("C", "D");
-    d.add_edge("B", "C");
+    d.add_edge("1", "0");
+    d.add_edge("0", "2");
+    d.add_edge("2", "1");
+    d.add_edge("0", "3");
+    d.add_edge("1", "4");
 
-    d.add_node("E");
+    d.add_node("5");
 
-    d.bfs("B");
+    d.bfs("0");
+    d.dfs("0");
 
     println!("\n\nDone ✅\n")
 }
